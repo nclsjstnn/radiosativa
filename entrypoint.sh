017b942a8aa85fb7d9ae1cd672dc6d558cb5a5c5
Nicolas Justiniano <n.justiniano@gmail.com>
2017-05-29 15:54:37 -0400

    dockercfg.encrypted & docker hub push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 # Create log dirs and files
-mkdir -p $( dirname $(cat /etc/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
-touch $( cat /etc/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
+mkdir -p $( dirname $(cat /etc/supervisor/conf.d/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// ) )
+touch $( cat /etc/supervisor/conf.d/supervisord.conf  | grep logfile= | grep "\.log" | sed s/.*logfile=// )
 
 # Then run supervisord
 /usr/bin/supervisord
